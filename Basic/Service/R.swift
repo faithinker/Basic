5cce835a8c29e6389adba95738c316887296c9be
파인원 김주협 <joohyupkim@pineone.com>
2021-12-15 14:49:23 +0900

    xcodeproj 폴더 누락으로 인해 깃클론이 안되는 문제 해결

--- Basic/Service/R.swift
+++ Basic/Service/R.swift
@@ -36,3 +36,14 @@ extension R.Color {
     /// #3DACF7 61 172 247
     static let blue = UIColor(61, 172, 247)
 }
+
+extension R.Color.Base {
+    static let input_field_bg = #colorLiteral(red: 0.8862745098, green: 0.8862745098, blue: 0.9019607843, alpha: 0.8)
+    static let mode_case_unselected = #colorLiteral(red: 0.9647058824, green: 0.968627451, blue: 0.9725490196, alpha: 1)
+    /// #60398E, RGB 96 57 142
+    static let mode_case_selected = #colorLiteral(red: 0.3764705882, green: 0.2235294118, blue: 0.5568627451, alpha: 1)
+    /// #949495, RGB 148 148 149
+    static let tab_num_unselected = #colorLiteral(red: 0.5803921569, green: 0.5803921569, blue: 0.5843137255, alpha: 1)
+    /// #E2E3E6, RGBA 226 227 230 0.3
+    static let tab_backgroundColor =  #colorLiteral(red: 0.8862745098, green: 0.8901960784, blue: 0.9019607843, alpha: 0.3)
+}
